d522afec8c830aec687123969a4fc5d4b2e813e0
Nicolas Sotelo <nicolassotelo@MacBook-Air-de-Nicolas.local>
2018-10-06 21:41:27 -0500

    Se crearon las apps de jobs y de blog , tambien se hizo la integracion con postgresql

--- jobs/models.py
+++ jobs/models.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    imagen= models.ImageField(upload_to='images')
+    descripcion = models.CharField(max_length=200)
